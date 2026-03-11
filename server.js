3108fc4a0725d6e0c313c06b8c1e1968709fc054
Aman Tripathi <legendaman1002@gmail.com>
2026-03-11 17:04:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/file-explorer';
-
 // Middleware
 app.use(cors());
 app.use(express.json());
