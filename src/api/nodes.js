ba8cc2614c90eb1d438a6dec810a5297e48cd14b
Aman Tripathi <legendaman1002@gmail.com>
2026-03-11 20:55:05 +0530

    Add files via upload

---
+++ src/api/nodes.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const api = axios.create({ baseURL: '/api' });
+
+export const getRootNodes = () => api.get('/nodes');
+export const getChildren = (parentId) => api.get(`/nodes/${parentId}/children`);
+export const createNode = (data) => api.post('/nodes', data);
+export const deleteNode = (id) => api.delete(`/nodes/${id}`);
