8a040d12b7399030ae8f1482a141a17ac912f75f
Aman Tripathi <legendaman1002@gmail.com>
2026-03-11 17:20:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/file-explorer';
+const MONGO_URI = "mongodb+srv://itsdemon100_db_user:YOUR_PASSWORD@cluster0.toiqp3q.mongodb.net/file-explorer?retryWrites=true&w=majority";
 // Middleware
 app.use(cors());
 app.use(express.json());
